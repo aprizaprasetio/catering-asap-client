d5733da5bd9642729675488976c43ad7d6073892
Abdvllahracata <abdullahracata104@gmail.com>
2023-03-01 08:09:30 +0700

    pop up animation form and adding profile picture into table foodDrink

--- src/components/organisms/FoodDrinkTableItem.js
+++ src/components/organisms/FoodDrinkTableItem.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import {
     TableRow, TableCell,
-    List, Stack, Collapse, IconButton, TextField, Typography, Checkbox,
+    List, Stack, Collapse, IconButton, TextField, Typography, Checkbox, Avatar
 } from '@mui/material'
 import { MoodRounded, SentimentNeutralRounded, MoodBadRounded, KeyboardArrowDown, KeyboardArrowUp, Save, Edit, Delete, }
     from '@mui/icons-material'
@@ -12,6 +12,8 @@ import { useTrigger } from 'commands/builders/commonBuilder'
 import ReactListItem from 'components/molecules/ReactListItem'
 import FoodDrinkTableCell from 'components/molecules/FoodDrinkTableCell'
 import CheckCircleIcon from '@mui/icons-material/CheckCircle'
+
+
 
 const FoodDrinkTableItem = ({ id, name, price, minOrder, description, image_Url }) => {
     const nilai = {
@@ -57,7 +59,7 @@ const FoodDrinkTableItem = ({ id, name, price, minOrder, description, image_Url 
         label: 'foto Profile',
         value: formikConfig.values.image_Url,
         onChange: formikConfig.handleChange,
-        isError: formikConfig.errors.image_Url
+        isError: formikConfig.errors.image_Url,
     }
 
     const nameConfig = {
@@ -96,24 +98,26 @@ const FoodDrinkTableItem = ({ id, name, price, minOrder, description, image_Url 
             <TableRow>
                 <TableCell width={1} sx={{ '& > *': { borderBottom: 'unset' } }}>
                     <IconButton
-                        aria-label="expand row"
-                        size="small"
+                        aria-label='expand row'
+                        size='small'
                         onClick={() => setOpen(!open)}>
                         {open ? <KeyboardArrowUp /> : <KeyboardArrowDown />}
 
                     </IconButton>
                 </TableCell>
-                <TableCell width={1} sx={{ textAlign: 'center' }} component="th">1</TableCell>
-                <FoodDrinkTableCell open={isEditMode} config={image_UrlConfig} />
+                <TableCell width={1} sx={{ textAlign: 'center' }} component='th'>1</TableCell>
+                <TableCell width={1} sx={{ textAlign: 'center' }} component='th'><Avatar
+                    alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+                </TableCell>
                 <FoodDrinkTableCell open={isEditMode} config={nameConfig} />
                 <FoodDrinkTableCell open={isEditMode} config={priceConfig} />
                 <FoodDrinkTableCell open={isEditMode} config={minOrderConfig} />
 
-                {/* <TableCell width={1} sx={{ textAlign: 'center' }} component="th" scope="row">1</TableCell>
+                {/* <TableCell width={1} sx={{ textAlign: 'center' }} component='th' scope='row'>1</TableCell>
                 {
                     isEditMode ?
                         (
-                            <TableCell sx={{ textAlign: 'center' }} component="th" scope="row" align="center">
+                            <TableCell sx={{ textAlign: 'center' }} component='th' scope='row' align='center">
                                 <TextField size='small' label='Nama' align='center' />
                             </TableCell>
                         ) : (
