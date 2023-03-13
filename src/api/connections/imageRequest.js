9b5056282f431b4e0a60ba1cb9d3cc53840ac654
aprizaprasetio <aprizaprasetio@gmail.com>
2023-03-13 10:41:31 +0700

    Read Image Hook FIx

--- src/api/connections/imageRequest.js
+++ src/api/connections/imageRequest.js
@@ -3,6 +3,7 @@ import { axios } from 'api/initiates/queryInitiate'
 const fetchImage = async FileName => {
     const image = await axios.get('/Image', {
         params: { FileName },
+        responseType: 'blob',
     })
 
     return image.data
--- src/api/hooks/imageHook.js
+++ src/api/hooks/imageHook.js
@@ -1,13 +1,21 @@
+import { useEffect } from 'react'
 import { useMutation } from '@tanstack/react-query'
+import { imageToBase64 } from 'commands/builders/imageBuilder'
 import { fetchImage } from 'api/connections/imageRequest'
 
-const useImageByPath = () => {
+const useImageByPath = path => {
     const imageMutation = useMutation({
-        mutationFn: fetchImage,
+        mutationFn: async () => {
+            const image = await fetchImage(path)
+            return await imageToBase64(image)
+        },
     })
+
+    useEffect(imageMutation.mutate, [])
 
     return imageMutation
 }
+
 
 // Example usage
 // const {data: image, isLoading: isImageLoading} = useImageByPath('payment/10-1-payment-2023-03-13.webp')
